f4146e0374a8c7268de14efba9d5eeee0e99ddc4
Nurhidayah Rahmat <hiixdayah2501@gmail.com>
2018-06-22 16:35:17 +0800

    Change1

--- app/src/main/java/com/example/mindy/simplymatch3/MainActivity.java
+++ app/src/main/java/com/example/mindy/simplymatch3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mindy.simplymatch3;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -18,13 +19,25 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setTitle("Collections");
+                    FragmentOne fragment = new FragmentOne() ;
+                    android.support.v4.app.FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction() ;
+                    fragmentTransaction1.replace(R.id.fram,fragment,"FragmentName") ;
+                    fragmentTransaction1.commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    setTitle("Add Outfits");
+                    FragmentTwo fragment2 = new FragmentTwo() ;
+                    android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction() ;
+                    fragmentTransaction2.replace(R.id.fram, fragment2,"FragmentName") ;
+                    fragmentTransaction2.commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    setTitle("Clothing Pieces");
+                    FragmentThree fragment3 = new FragmentThree() ;
+                    android.support.v4.app.FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction() ;
+                    fragmentTransaction3.replace(R.id.fram,fragment3,"FragmentName") ;
+                    fragmentTransaction3.commit();
                     return true;
             }
             return false;
@@ -39,6 +52,13 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //when app starts fragment one will be displayed
+        setTitle("Collections");
+        FragmentOne fragment = new FragmentOne() ;
+        android.support.v4.app.FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction() ;
+        fragmentTransaction1.replace(R.id.fram,fragment,"FragmentName") ;
+        fragmentTransaction1.commit();
     }
 
 }
